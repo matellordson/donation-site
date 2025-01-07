84daab49c65448d8d1eda2bb7d0a4d5cdc9c0777
matellorson <snr.mn.lordson@gmail.com>
2025-01-07 11:04:03 +0100

    fix

--- components/block/signup-form.tsx
+++ components/block/signup-form.tsx
@@ -17,7 +17,7 @@ export function SignupForm({
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Create an account</h1>
                 <p className="text-balance text-muted-foreground">
-                  Create your Acme Inc account
+                  Register your Acme Inc account
                 </p>
               </div>
               <div className="grid gap-2">
@@ -34,7 +34,7 @@ export function SignupForm({
                 <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                Sign up
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
