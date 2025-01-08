9f988ac8c1da760fe3afea66234eae84b3c2a2e0
matellorson <snr.mn.lordson@gmail.com>
2025-01-09 00:22:24 +0100

    tweak

--- app/ways-to-give/page.tsx
+++ app/ways-to-give/page.tsx
@@ -2,7 +2,7 @@ export default function waysToGive() {
   const donationOptions = [
     {
       title: "Help Turky and Syria Earthquake Relief Fund",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia deserunt beatae sapiente necessitatibus laudantium qui similique laborum voluptatibus, et reiciendis consequatur nihil, alias reprehenderit itaque blanditiis illum dignissimos perspiciatis quo amet. Nemo culpa ea sequi, vel quae provident tempore quaerat omnis esse ipsa incidunt ipsum, facilis hic distinctio officia velit.",
+      desc: "since 2006, SOIL has been working urban to provide basic aminities to those who suffer",
       raised: "$45,669",
       total: "$300,000",
       progress: "30%",
@@ -28,17 +28,20 @@ export default function waysToGive() {
 
   return (
     <div className="mx-auto max-w-sm px-3 py-10 lg:max-w-5xl lg:px-0">
-      <div className="grid grid-cols-2 gap-2 lg:grid-cols-5">
+      <div className="grid grid-cols-2 gap-2 lg:grid-cols-4">
         {donationOptions.map((option) => (
           <div
-            className="flex h-[250px] w-full flex-col items-center justify-between rounded border border-muted p-2 shadow-sm lg:h-[300px]"
+            className="lg:[300px] flex h-[250px] w-full flex-col items-center justify-between rounded border border-muted p-2 shadow-sm lg:h-[300px]"
             key={option.title}
           >
             <div className="h-[40%] w-full rounded bg-muted shadow-sm"></div>
             <div className="h-[60%] w-full py-3">
               {/* truncate to only two line */}
-              <p className="text-[11px] font-bold tracking-wide lg:text-sm">
+              <p className="line-clamp-2 text-[11px] font-bold tracking-wide lg:text-sm">
                 {option.title}
+              </p>
+              <p className="line-clamp-2 text-[11px] text-muted-foreground lg:text-sm">
+                {option.desc}
               </p>
             </div>
           </div>
