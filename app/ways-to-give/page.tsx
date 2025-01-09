a41b8d1f9fe2c51df7eee00392a8bce51b3ba036
matellorson <snr.mn.lordson@gmail.com>
2025-01-09 01:03:40 +0100

    tweak

--- app/ways-to-give/page.tsx
+++ app/ways-to-give/page.tsx
@@ -1,3 +1,5 @@
+import { Progress } from "@/components/ui/progress";
+
 export default function waysToGive() {
   const donationOptions = [
     {
@@ -5,7 +7,7 @@ export default function waysToGive() {
       desc: "since 2006, SOIL has been working urban to provide basic aminities to those who suffer",
       raised: "$45,669",
       total: "$300,000",
-      progress: "30%",
+      progress: 79,
       supporters: "45,000",
     },
     {
@@ -13,7 +15,7 @@ export default function waysToGive() {
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia deserunt beatae sapiente necessitatibus laudantium qui similique laborum voluptatibus, et reiciendis consequatur nihil, alias reprehenderit itaque blanditiis illum dignissimos perspiciatis quo amet. Nemo culpa ea sequi, vel quae provident tempore quaerat omnis esse ipsa incidunt ipsum, facilis hic distinctio officia velit.",
       raised: "$2,669",
       total: "$1,000",
-      progress: "60%",
+      progress: 60,
       supporters: "4,000",
     },
     {
@@ -21,8 +23,8 @@ export default function waysToGive() {
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia deserunt beatae sapiente necessitatibus laudantium qui similique laborum voluptatibus, et reiciendis consequatur nihil, alias reprehenderit itaque blanditiis illum dignissimos perspiciatis quo amet. Nemo culpa ea sequi, vel quae provident tempore quaerat omnis esse ipsa incidunt ipsum, facilis hic distinctio officia velit.",
       raised: "$2,669",
       total: "$1,000",
-      progress: "60%",
-      supporters: "4,000",
+      progress: 20,
+      supporters: "1,000",
     },
   ];
 
@@ -34,15 +36,40 @@ export default function waysToGive() {
             className="lg:[300px] flex h-[250px] w-full flex-col items-center justify-between rounded border border-muted p-2 shadow-sm lg:h-[300px]"
             key={option.title}
           >
-            <div className="h-[40%] w-full rounded bg-muted shadow-sm"></div>
-            <div className="h-[60%] w-full py-3">
-              {/* truncate to only two line */}
-              <p className="line-clamp-2 text-[11px] font-bold tracking-wide lg:text-sm">
-                {option.title}
-              </p>
-              <p className="line-clamp-2 text-[11px] text-muted-foreground lg:text-sm">
-                {option.desc}
-              </p>
+            <div className="h-[49%] w-full rounded bg-muted shadow-sm"></div>
+            <div className="h-[51%] w-full py-3">
+              <>
+                <p className="line-clamp-2 text-[12px] font-bold tracking-wide lg:text-sm">
+                  {option.title}
+                </p>
+                <p className="line-clamp-2 text-[11px] text-muted-foreground">
+                  {option.desc}
+                </p>
+              </>
+              <div className="mt-2 space-y-px">
+                <p className="text-[11px] font-bold">
+                  {option.raised}
+                  <span className="font-normal text-muted-foreground">
+                    {" / "}
+                    {option.total}
+                  </span>
+                </p>
+                <Progress value={option.progress} />
+                <div className="flex gap-x-1 pt-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-3 text-pink-600"
+                  >
+                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                  </svg>
+
+                  <p className="text-[10px] text-muted-foreground">
+                    {option.supporters} Supporters
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         ))}
