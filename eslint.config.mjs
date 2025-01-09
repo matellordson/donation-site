4c20c3a13a00a7186360ca83c1a9448b1fad2750
matellorson <snr.mn.lordson@gmail.com>
2025-01-09 15:50:55 +0100

    tweak

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.config(["next/core-web-vitals", "next/typescript"], {
+  ...compat.config("next/core-web-vitals", "next/typescript", {
     extends: ["next"],
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
