109788626077518be623f250486c222caff0335b
matellorson <snr.mn.lordson@gmail.com>
2025-01-10 12:21:48 +0100

    tweak

--- app/ways-to-give/[slug]/donate/page.tsx
+++ app/ways-to-give/[slug]/donate/page.tsx
@@ -22,7 +22,7 @@ export default function Donate() {
       </div>
       <div className="relative hidden bg-muted lg:block">
         <img
-          src="/placeholder.svg"
+          src="/donate.jpg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -60,7 +60,7 @@ const CommandList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive.List
     ref={ref}
-    className={cn("max-h-[160px] overflow-y-auto overflow-x-hidden", className)}
+    className={cn("max-h-[157px] overflow-y-auto overflow-x-hidden", className)}
     {...props}
   />
 ));
