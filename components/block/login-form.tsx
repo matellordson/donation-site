e4e528ae2f46f4e9cb0880532eba48ef1ade414d
matellorson <snr.mn.lordson@gmail.com>
2025-01-11 12:32:19 +0100

    added navbar to auth pages

--- components/block/login-form.tsx
+++ components/block/login-form.tsx
@@ -4,63 +4,75 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { login } from "@/app/auth/actions";
+import Navbar from "./navbar";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="overflow-hidden">
-        <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8" action={login}>
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className="text-balance text-muted-foreground">
-                  Login to make a Donation
-                </p>
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+    <div className="">
+      <Navbar />
+      <div className={cn("mt-14 flex flex-col gap-6", className)} {...props}>
+        <Card className="overflow-hidden">
+          <CardContent className="grid p-0 md:grid-cols-2">
+            <form className="p-6 md:p-8" action={login}>
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back</h1>
+                  <p className="text-balance text-muted-foreground">
+                    Login to make a Donation
+                  </p>
                 </div>
-                <Input id="password" name="password" type="password" required />
-              </div>
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                  </div>
+                  <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
 
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="/auth/signup" className="underline underline-offset-4">
-                  Sign up
-                </a>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a
+                    href="/auth/signup"
+                    className="underline underline-offset-4"
+                  >
+                    Sign up
+                  </a>
+                </div>
               </div>
+            </form>
+            <div className="relative hidden bg-muted md:block">
+              <img
+                src="/login.jpg"
+                alt="Image"
+                className="absolute inset-0 h-full w-full object-cover grayscale dark:brightness-[0.2] dark:grayscale"
+              />
             </div>
-          </form>
-          <div className="relative hidden bg-muted md:block">
-            <img
-              src="/login.jpg"
-              alt="Image"
-              className="absolute inset-0 h-full w-full object-cover grayscale dark:brightness-[0.2] dark:grayscale"
-            />
-          </div>
-        </CardContent>
-      </Card>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+          </CardContent>
+        </Card>
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
       </div>
     </div>
   );
--- components/block/signup-form.tsx
+++ components/block/signup-form.tsx
@@ -4,63 +4,75 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signup } from "@/app/auth/actions";
+import Navbar from "./navbar";
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card className="overflow-hidden">
-        <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8" action={signup}>
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Create an account</h1>
-                <p className="text-balance text-muted-foreground">
-                  Register an account to make Donation
-                </p>
+    <div className="">
+      <Navbar />
+      <div className={cn("mt-14 flex flex-col gap-6", className)} {...props}>
+        <Card className="overflow-hidden">
+          <CardContent className="grid p-0 md:grid-cols-2">
+            <form className="p-6 md:p-8" action={signup}>
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Create an account</h1>
+                  <p className="text-balance text-muted-foreground">
+                    Register an account to make Donation
+                  </p>
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    id="password"
+                    type="password"
+                    name="password"
+                    required
+                  />
+                  <p className="text-sm text-muted-foreground">
+                    Minimum 6, recommended 8 or more.
+                  </p>
+                </div>
+                <Button type="submit" className="w-full">
+                  Sign up
+                </Button>
+                <div className="text-center text-sm">
+                  Already have an account?{" "}
+                  <a
+                    href="/auth/login"
+                    className="underline underline-offset-4"
+                  >
+                    Login
+                  </a>
+                </div>
               </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" name="password" required />
-                <p className="text-sm text-muted-foreground">
-                  Minimum 6, recommended 8 or more.
-                </p>
-              </div>
-              <Button type="submit" className="w-full">
-                Sign up
-              </Button>
-              <div className="text-center text-sm">
-                Already have an account?{" "}
-                <a href="/auth/login" className="underline underline-offset-4">
-                  Login
-                </a>
-              </div>
+            </form>
+            <div className="relative hidden bg-muted md:block">
+              <img
+                src="/signup.jpg"
+                alt="Image"
+                className="absolute inset-0 h-full w-full object-cover grayscale dark:brightness-[0.2] dark:grayscale"
+              />
             </div>
-          </form>
-          <div className="relative hidden bg-muted md:block">
-            <img
-              src="/signup.jpg"
-              alt="Image"
-              className="absolute inset-0 h-full w-full object-cover grayscale dark:brightness-[0.2] dark:grayscale"
-            />
-          </div>
-        </CardContent>
-      </Card>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+          </CardContent>
+        </Card>
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
       </div>
     </div>
   );
