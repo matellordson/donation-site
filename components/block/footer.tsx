f1abac82008fc9292e468cbb28772fc3833c1794
matellorson <snr.mn.lordson@gmail.com>
2025-01-12 23:33:19 +0100

    tweak

--- components/block/footer.tsx
+++ components/block/footer.tsx
@@ -5,9 +5,9 @@ export default function Footer({ className }: { className?: string }) {
     <footer
       className={`max-w-mx-auto mx-auto h-full w-full max-w-sm px-3 py-3 lg:max-w-6xl lg:px-0 ${className}`}
     >
-      <div className="w-full rounded-xl bg-primary p-5 text-primary-foreground">
+      <div className="w-full rounded-xl bg-primary p-5 text-primary-foreground lg:px-6">
         <div className="">
-          <div className="flex items-center justify-between">
+          <div className="flex-col items-center justify-between space-y-5 lg:flex lg:space-y-0">
             <div className="flex-shrink-0 space-y-2">
               <Link href="/" className="flex items-center">
                 <svg
@@ -27,41 +27,42 @@ export default function Footer({ className }: { className?: string }) {
               </Link>
               <p className="text-xs">Your Compassion Can Rewrite a Story.</p>
             </div>
-            <div className="flex items-center justify-start lg:justify-center"></div>
-            <div className="flex flex-col text-xs">
-              <Link href="/about-us" className="rounded-md px-3 py-1">
-                About Us
-              </Link>
-              <Link href="/our-work" className="rounded-md px-3 py-1">
-                Our Work
-              </Link>
-              <Link href="/ways-to-give" className="rounded-md px-3 py-1">
-                Ways to Give
-              </Link>
-            </div>
+            <div className="flex items-center justify-start gap-x-5 lg:justify-center">
+              <div className="flex flex-col text-xs">
+                <Link href="/about-us" className="rounded-md px-3 py-1">
+                  About Us
+                </Link>
+                <Link href="/our-work" className="rounded-md px-3 py-1">
+                  Our Work
+                </Link>
+                <Link href="/ways-to-give" className="rounded-md px-3 py-1">
+                  Ways to Give
+                </Link>
+              </div>
 
-            <div className="flex flex-col text-xs">
-              <Link href="#" className="rounded-md px-3 py-1">
-                Our Impact
-              </Link>
-              <Link href="#" className="rounded-md px-3 py-1">
-                Contact Us
-              </Link>
-              <Link href="#" className="rounded-md px-3 py-1">
-                Partnership
-              </Link>
-            </div>
+              <div className="flex flex-col text-xs">
+                <Link href="#" className="rounded-md px-3 py-1">
+                  Our Impact
+                </Link>
+                <Link href="#" className="rounded-md px-3 py-1">
+                  Contact Us
+                </Link>
+                <Link href="#" className="rounded-md px-3 py-1">
+                  Partnership
+                </Link>
+              </div>
 
-            <div className="flex flex-col text-xs">
-              <Link href="#" className="rounded-md px-3 py-1">
-                X
-              </Link>
-              <Link href="#" className="rounded-md px-3 py-1">
-                Instagram
-              </Link>
-              <Link href="#" className="rounded-md px-3 py-1">
-                Facebook
-              </Link>
+              <div className="flex flex-col text-xs">
+                <Link href="#" className="rounded-md px-3 py-1">
+                  X
+                </Link>
+                <Link href="#" className="rounded-md px-3 py-1">
+                  Instagram
+                </Link>
+                <Link href="#" className="rounded-md px-3 py-1">
+                  Facebook
+                </Link>
+              </div>
             </div>
           </div>
         </div>
