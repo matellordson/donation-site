a095924d909ad668c9327d4a3aaf5898cd15629c
matellorson <snr.mn.lordson@gmail.com>
2025-01-12 23:18:44 +0100

    doing footer stuff

--- components/block/footer.tsx
+++ components/block/footer.tsx
@@ -5,25 +5,64 @@ export default function Footer({ className }: { className?: string }) {
     <footer
       className={`max-w-mx-auto mx-auto h-full w-full max-w-sm px-3 py-3 lg:max-w-6xl lg:px-0 ${className}`}
     >
-      <div className="h-20 w-full rounded-xl bg-primary p-5 text-primary-foreground">
+      <div className="w-full rounded-xl bg-primary p-5 text-primary-foreground">
         <div className="">
-          <div className="flex-shrink-0">
-            <Link href="/" className="flex items-center">
-              <svg
-                className="h-8 w-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                />
-              </svg>
-              <span className="ml-2 text-lg font-bold">J&H Foundation</span>
-            </Link>
+          <div className="flex items-center justify-between">
+            <div className="flex-shrink-0 space-y-2">
+              <Link href="/" className="flex items-center">
+                <svg
+                  className="h-8 w-8"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
+                  />
+                </svg>
+                <span className="ml-2 text-lg font-bold">J&H Foundation</span>
+              </Link>
+              <p className="text-xs">Your Compassion Can Rewrite a Story.</p>
+            </div>
+            <div className="flex items-center justify-start lg:justify-center"></div>
+            <div className="flex flex-col text-xs">
+              <Link href="/about-us" className="rounded-md px-3 py-1">
+                About Us
+              </Link>
+              <Link href="/our-work" className="rounded-md px-3 py-1">
+                Our Work
+              </Link>
+              <Link href="/ways-to-give" className="rounded-md px-3 py-1">
+                Ways to Give
+              </Link>
+            </div>
+
+            <div className="flex flex-col text-xs">
+              <Link href="#" className="rounded-md px-3 py-1">
+                Our Impact
+              </Link>
+              <Link href="#" className="rounded-md px-3 py-1">
+                Contact Us
+              </Link>
+              <Link href="#" className="rounded-md px-3 py-1">
+                Partnership
+              </Link>
+            </div>
+
+            <div className="flex flex-col text-xs">
+              <Link href="#" className="rounded-md px-3 py-1">
+                X
+              </Link>
+              <Link href="#" className="rounded-md px-3 py-1">
+                Instagram
+              </Link>
+              <Link href="#" className="rounded-md px-3 py-1">
+                Facebook
+              </Link>
+            </div>
           </div>
         </div>
       </div>
