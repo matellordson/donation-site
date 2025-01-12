eac7e7f356be7b4794a5e4af682baa71dfe8cb31
matellorson <snr.mn.lordson@gmail.com>
2025-01-12 22:47:51 +0100

    cmt

--- components/block/footer.tsx
+++ components/block/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer({ className }: { className: string }) {
   return (
     <footer
-      className={`max-w-mx-auto mx-auto w-full max-w-sm lg:max-w-6xl ${className}`}
+      className={`max-w-mx-auto mx-auto h-full w-full max-w-sm lg:max-w-6xl ${className}`}
     >
       <div className="h-20 w-full bg-primary"></div>
     </footer>
