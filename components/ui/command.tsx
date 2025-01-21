c2f8dc2b3eca4f7431d403704d2fde5b147eb18e
matellorson <snr.mn.lordson@gmail.com>
2025-01-21 19:17:52 +0100

    tweak

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -60,7 +60,7 @@ const CommandList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <CommandPrimitive.List
     ref={ref}
-    className={cn("max-h-[170px] overflow-y-auto overflow-x-hidden", className)}
+    className={cn("max-h-[160px] overflow-y-auto overflow-x-hidden", className)}
     {...props}
   />
 ));
