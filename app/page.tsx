bbf8f970f6e71f3a2748e756987d00d84806c135
matellorson <snr.mn.lordson@gmail.com>
2025-01-29 22:14:14 +0100

    tweak

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import DonateButton from "@/components/block/donate-button";
 import Image from "next/image";
-import { CardHeader, Card as Cards } from "@/components/ui/card";
+import {
+  CardContent,
+  CardHeader,
+  Card as Cards,
+  CardTitle,
+} from "@/components/ui/card";
+import { Heart, PersonStanding, Sprout } from "lucide-react";
 
 export default function Home() {
   return (
@@ -37,6 +43,7 @@ export default function Home() {
           </DonateButton>
         </div>
       </div>
+      <hr className="mt-8 h-1 w-full border-2 border-dashed border-secondary" />
       <Card />
     </div>
   );
@@ -44,16 +51,53 @@ export default function Home() {
 
 function Card() {
   return (
-    <div className="mt-7">
-      <p className="text-2xl font-bold capitalize">
-        Lorem ipsum dolor sit amet.
-      </p>
+    <div className="mt-4">
+      <p className="text-2xl font-bold capitalize">Unlock the Joy of Giving</p>
       <p className="text-muted-foreground">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, hic.
+        Giving spreads hope and changes lives. Every act of kindness matters!
       </p>
-      <div className="">
-        <Cards>
-          <CardHeader></CardHeader>
+      <div className="mt-3 grid grid-cols-1 gap-3 lg:grid-cols-3">
+        <Cards className="bg-accent">
+          <CardHeader className="space-y-3">
+            <div className="h-fit w-fit rounded-full bg-secondary p-2">
+              <Sprout className="text-accent-foreground" />
+            </div>
+            <CardTitle className="text-primary">Giving Changes Lives</CardTitle>
+          </CardHeader>
+          <CardContent className="prose">
+            Every donation, big or small, has the power to transform someone's
+            life. Whether it's food, clothing, money, or time, your generosity
+            can bring hope and joy to those in need.
+          </CardContent>
+        </Cards>
+        <Cards className="bg-accent">
+          <CardHeader className="space-y-3">
+            <div className="h-fit w-fit rounded-full bg-secondary p-2">
+              <Heart className="text-accent-foreground" />
+            </div>
+            <CardTitle className="text-primary">
+              Donate, Inspire, Impact
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="prose">
+            Your act of giving doesn’t just help others—it inspires more
+            kindness and creates a ripple effect of positive change. When you
+            donate, you become part of something bigger, making a real impact in
+            the world.
+          </CardContent>
+        </Cards>
+        <Cards className="bg-accent">
+          <CardHeader className="space-y-3">
+            <div className="h-fit w-fit rounded-full bg-secondary p-2">
+              <PersonStanding className="text-accent-foreground" />
+            </div>
+            <CardTitle className="text-primary">Be the Difference</CardTitle>
+          </CardHeader>
+          <CardContent className="prose">
+            Change starts with you. Your willingness to give can mean shelter
+            for the homeless, food for the hungry, or education for a child.
+            Step forward and be the difference today!
+          </CardContent>
         </Cards>
       </div>
     </div>
