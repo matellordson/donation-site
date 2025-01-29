7ab2a06e8a03b497f18e16e6e22039962f834d50
matellorson <snr.mn.lordson@gmail.com>
2025-01-29 21:25:25 +0100

    tweak

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="mx-auto mt-20 max-w-7xl px-3 lg:px-0">
-      <div className="relative flex h-[430px] w-full items-end justify-start overflow-hidden rounded-xl bg-gray-600 shadow-sm">
+      <div className="relative flex h-[450px] w-full items-end justify-start overflow-hidden rounded-xl bg-gray-600 shadow-sm lg:h-[500px]">
         <Image
           src={"/banner.jpg"}
           className="absolute h-full w-full object-cover mix-blend-overlay grayscale"
@@ -14,7 +14,7 @@ export default function Home() {
           height={430}
           priority
         />
-        <div className="flex w-full flex-col items-start justify-center text-gray-100 drop-shadow-2xl lg:flex-row lg:items-end lg:justify-between">
+        <div className="flex w-full flex-col items-start justify-center text-gray-50 drop-shadow-2xl lg:flex-row lg:items-end lg:justify-between">
           <div className="flex items-end gap-x-3 pl-3 lg:pl-5">
             <p className="relative top-1 p-0 text-[75px] font-bold tracking-tighter lg:top-10 lg:text-[150px]">
               Donate
