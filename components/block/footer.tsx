ad0d42521e145bda0349b6e614260d4d73a5f09b
matellorson <snr.mn.lordson@gmail.com>
2025-03-05 20:01:10 +0100

    cmt

---
+++ components/block/footer.tsx
@@ -0,0 +1,101 @@
+import Link from "next/link";
+import { Facebook, Twitter, Instagram, Github, Zap } from "lucide-react";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <div className="p-2">
+      <footer className="mx-auto w-full max-w-sm rounded-xl bg-primary py-10 text-primary-foreground lg:max-w-7xl">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col space-y-8 md:flex-row md:justify-between md:space-y-0">
+            <div className="flex items-center">
+              <Image
+                src={"/logo2.png"}
+                alt="logo"
+                height={90}
+                width={90}
+                className="relative right-4"
+              />
+              <div className="relative right-7">
+                <h3 className="text-lg font-bold">J&H Foundation</h3>
+                <p className="text-sm">
+                  &copy; 2025 J&H Foundation. All rights reserved.
+                </p>
+              </div>
+            </div>
+            <nav className="flex flex-col space-y-4 md:flex-row md:space-x-6 md:space-y-0">
+              <Link href="/" className="underline-offset-4 hover:underline">
+                Home
+              </Link>
+              <Link
+                href="/about-us"
+                className="underline-offset-4 hover:underline"
+              >
+                About Us
+              </Link>
+              <Link
+                href="/contact-us"
+                className="underline-offset-4 hover:underline"
+              >
+                Contact Us
+              </Link>
+              <Link
+                href="/our-work"
+                className="underline-offset-4 hover:underline"
+              >
+                Our Work
+              </Link>
+              <Link
+                href="/partners"
+                className="underline-offset-4 hover:underline"
+              >
+                Partnership
+              </Link>
+              <Link
+                href="/stories"
+                className="underline-offset-4 hover:underline"
+              >
+                stories
+              </Link>
+              <Link
+                href="/terms"
+                className="underline-offset-4 hover:underline"
+              >
+                Terms of Service
+              </Link>
+            </nav>
+            <div className="flex space-x-4">
+              <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                <Facebook size={20} />
+                <span className="sr-only">Facebook</span>
+              </a>
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                <Twitter size={20} />
+                <span className="sr-only">Twitter</span>
+              </a>
+              <a
+                href="https://instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline"
+              >
+                <Instagram size={20} />
+                <span className="sr-only">Instagram</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ components/block/navigation-menu.tsx
@@ -0,0 +1,167 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { Bolt } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { SignedIn } from "@clerk/nextjs";
+
+const overview: { title: string; href: string; description: string }[] = [
+  {
+    title: "About Us",
+    href: "/about-us",
+    description:
+      "Discover our mission, values, and the team working to create positive change in the world.",
+  },
+  {
+    title: "Appeal for Donation",
+    href: "/appeal-for-donation",
+    description: "Request a donation today and get the support you need!",
+  },
+  {
+    title: "Our Work",
+    href: "/our-work",
+    description:
+      "Learn about the programs and projects you’re helping fund to create lasting impact.",
+  },
+  {
+    title: "Partnership",
+    href: "/partners",
+    description:
+      "Learn about the programs and projects you’re helping fund to create lasting impact.",
+  },
+  {
+    title: "stories",
+    href: "/stories",
+    description:
+      "Read powerful stories from individuals and communities whose lives have been transformed.",
+  },
+  {
+    title: "Terms of Service",
+    href: "/terms",
+    description:
+      "Understand the terms governing your use of our website and donation process.",
+  },
+];
+
+export function NavigationMenuDemo() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Get involved</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild className="">
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      J&H Foundation
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Donating is a powerful way to create change. Your support
+                      brings hope and resources to those in need, making a real
+                      difference in their lives.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/about-us" title="About Us">
+                Learn about our mission, values, and the change we’re creating
+                together.
+              </ListItem>
+              <ListItem href="/ways-to-give" title="Ways to Give">
+                Explore donation causes and make an impact
+              </ListItem>
+              <ListItem href="/appeal-for-donation" title="Appeal for Donation">
+                Request a donation today and get the support you need!
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Overview</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {overview.map((overview) => (
+                <ListItem
+                  key={overview.title}
+                  title={overview.title}
+                  href={overview.href}
+                >
+                  {overview.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/contact-us" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Contact us
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/ways-to-give" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Donate
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <SignedIn>
+          <NavigationMenuItem>
+            <Link
+              href="https://billing.stripe.com/p/login/5kAcQzeiK1P3e1aaEE"
+              legacyBehavior
+              passHref
+            >
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Billings
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </SignedIn>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
